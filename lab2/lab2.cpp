bedcf61bb2850e85e40bd366e9bd0ccf3aa6931d
oli001 <oli001@ucr.edu>
2015-04-17 02:44:36 +0000

    added the lab files

---
+++ lab2/lab2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <forward_list>
+#include <list>
+
+using namespace std;
+
+int primeCount(forward_list<int> lst);
+
+forward_list <Type> listCopy(forward_list <Type> L, forward_list <Type> P);
+
+void printLots(forward_list <Type> L, forward_list <int> P);
+
+int main()
+{
+    return 0;
+}
